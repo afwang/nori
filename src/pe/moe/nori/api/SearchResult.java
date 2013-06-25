6f2e9a416f814c2a49fa3e1bf8d69967d002a96e
vomitcuddle <shinku@dollbooru.org>
2013-06-25 02:53:57 +0100

    Parcelable search results

--- src/pe/moe/nori/api/SearchResult.java
+++ src/pe/moe/nori/api/SearchResult.java
@@ -5,13 +5,59 @@
  */
 package pe.moe.nori.api;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.ArrayList;
 
-public class SearchResult {
+public class SearchResult implements Parcelable {
+  /** Class loader used when deserializing from {@link Parcel}. */
+  public static final Parcelable.Creator<SearchResult> CREATOR = new Parcelable.Creator<SearchResult>() {
+    public SearchResult createFromParcel(Parcel in) {
+      // Use the parcel constructor.
+      return new SearchResult(in);
+    }
+
+    @Override
+    public SearchResult[] newArray(int size) {
+      // Return new array.
+      return new SearchResult[size];
+    }
+  };
   /** List of images */
   public ArrayList<Image> images = new ArrayList<Image>();
   /** Image count, across all pages */
   public long count;
   /** Offset, used for paging */
   public long offset;
+
+  /** Default constructor */
+  public SearchResult() {
+  }
+
+  /**
+   * Constructor used for deserializing from {@link Parcel}
+   * @param in {@link Parcel} to read values from
+   */
+  protected SearchResult(Parcel in) {
+    // Read values from parcel.
+    in.readList(images, null);
+    count = in.readLong();
+    offset = in.readLong();
+  }
+
+  @Override
+  public int describeContents() {
+    // Not used.
+    return 0;
+  }
+
+  @Override
+  public void writeToParcel(Parcel dest, int flags) {
+    // Write values to parcel.
+    dest.writeList(images);
+    dest.writeLong(count);
+    dest.writeLong(offset);
+  }
+
 }
