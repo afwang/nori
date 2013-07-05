380d34f65be7d329df2e793b08c8b5abbe7b5381
vomitcuddle <shinku@dollbooru.org>
2013-07-05 19:38:21 +0100

    Create volley ImageLoader

--- src/pe/moe/nori/ImageViewerActivity.java
+++ src/pe/moe/nori/ImageViewerActivity.java
@@ -1,17 +1,46 @@
 package pe.moe.nori;
 
+import android.graphics.Bitmap;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.util.LruCache;
 import com.actionbarsherlock.app.SherlockActivity;
 import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.Volley;
 import pe.moe.nori.api.BooruClient;
 import pe.moe.nori.api.SearchResult;
 import pe.moe.nori.providers.ServiceSettingsProvider;
 
 public class ImageViewerActivity extends SherlockActivity {
+  /** Current SearchResult */
   private SearchResult mSearchResult;
+  /** HTTP request queue */
   private RequestQueue mRequestQueue;
+  /** API client */
   private BooruClient mBooruClient;
+  /** Bitmap LRU cache */
+  private LruCache<String, Bitmap> mBitmapLruCache = new LruCache<String, Bitmap>(4096) {
+    @Override
+    protected int sizeOf(String key, Bitmap value) {
+      // Convert size to kilobytes.
+      return (int) ((long) value.getRowBytes() * (long) value.getHeight() / 1024);
+    }
+  };
+  /** Volley image cache */
+  private ImageLoader.ImageCache mImageCache = new ImageLoader.ImageCache() {
+    @Override
+    public Bitmap getBitmap(String url) {
+      return mBitmapLruCache.get(url);
+    }
+
+    @Override
+    public void putBitmap(String url, Bitmap bitmap) {
+      mBitmapLruCache.put(url, bitmap);
+    }
+  };
+  /** Volley image lazyloader */
+  private ImageLoader mImageLoader;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +51,13 @@ public class ImageViewerActivity extends SherlockActivity {
     mRequestQueue = Volley.newRequestQueue(this);
     mBooruClient = ServiceSettingsProvider.ServiceSettings.createClient(mRequestQueue,
         getIntent().<ServiceSettingsProvider.ServiceSettings>getParcelableExtra("pe.moe.nori.api.Settings"));
+    mImageLoader = new ImageLoader(mRequestQueue, mImageCache);
   }
 
+  @Override
+  public void onLowMemory() {
+    super.onLowMemory();
+    // Trim bitmap cache to reduce memory usage.
+    mBitmapLruCache.trimToSize(32);
+  }
 }
