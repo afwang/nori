c1a80f39d9f11de47a781aec1981663fab992635
vomitcuddle <shinku@dollbooru.org>
2013-07-07 03:53:16 +0100

    Start ServiceSettingsActivity from SettingsActivity

---
+++ src/pe/moe/nori/ServiceSettingsActivity.java
@@ -0,0 +1,12 @@
+package pe.moe.nori;
+
+import android.os.Bundle;
+import com.actionbarsherlock.app.SherlockListActivity;
+
+public class ServiceSettingsActivity extends SherlockListActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
+}
