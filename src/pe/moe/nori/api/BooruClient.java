1a8778581608ac7f7dcff4356d0b0b1df5fdaa39
vomitcuddle <shinku@dollbooru.org>
2013-07-18 16:15:59 +0100

    Refactor DanbooruLegacy into an abstract class

--- src/pe/moe/nori/api/BooruClient.java
+++ src/pe/moe/nori/api/BooruClient.java
@@ -4,30 +4,29 @@ import com.android.volley.Request;
 import com.android.volley.Response.ErrorListener;
 import com.android.volley.Response.Listener;
 
-/**
- * Imageboard service client interface.
- */
+/** Imageboard service client interface. */
 public interface BooruClient {
-  
+
   /**
    * Checks if current API endpoint requires authentication.
-   * 
+   *
    * @return Return true if current endpoint uses HTTP Basic Auth.
    */
   public boolean requiresAuthentication();
-  
+
   /**
    * Get default query to search for when application is first launched.
-   * 
+   *
    * @return Default query. Should be something like "rating: safe"
    */
   public String getDefaultQuery();
+
   /**
    * Fetch a list of {@link Image}s using Android Volley.
-   * 
-   * @param tags Tags to search for. Any tag combination that works on the web should work here
-   * @param pid Page number
-   * @param listener Listener to receive the {@link SearchResult} response
+   *
+   * @param tags          Tags to search for. Any tag combination that works on the web should work here
+   * @param pid           Page number
+   * @param listener      Listener to receive the {@link SearchResult} response
    * @param errorListener Error listener, or null to ignore errors
    * @return Android Volley {@link Request}
    */
@@ -35,9 +34,9 @@ public interface BooruClient {
 
   /**
    * Fetch a list of {@link Image}s using Android Volley.
-   * 
-   * @param tags Tags to search for. Any tag combination that works on the web should work here
-   * @param listener Listener to receive the {@link SearchResult} response
+   *
+   * @param tags          Tags to search for. Any tag combination that works on the web should work here
+   * @param listener      Listener to receive the {@link SearchResult} response
    * @param errorListener Error listener, or null to ignore errors
    * @return Android Volley {@link Request}
    */
