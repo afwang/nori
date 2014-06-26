c32e4c5b550dfea0a5b6e36fda1d12ff6406d4b9
vomitcuddle <shinku@dollbooru.org>
2014-06-26 05:21:28 +0100

    Create abstract class for API clients

--- src/main/java/com/cuddlesoft/nori/api/clients/BaseAPIClient.java
+++ src/main/java/com/cuddlesoft/nori/api/clients/BaseAPIClient.java
@@ -6,6 +6,9 @@
 
 package com.cuddlesoft.nori.api.clients;
 
+import com.cuddlesoft.nori.api.SearchResult;
+
+import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -41,6 +44,55 @@ public abstract class BaseAPIClient {
 
     // No ID matched.
     return null;
+  }
+
+  /**
+   * Get default query for this API.
+   * Use "rating:safe" to only show SFW when the app is launched.
+   * Ignored if user sets a custom default query in the settings.
+   *
+   * @return Default query string for this API.
+   */
+  public String getDefaultQuery() {
+    return "rating:safe";
+  }
+
+  /**
+   * Fetch first page of results containing images with the given set of tags.
+   *
+   * @param tags Search query. A space-separated list of tags.
+   * @return A {@link com.cuddlesoft.nori.api.SearchResult} containing a set of Images.
+   * @throws IOException Network error.
+   */
+  public abstract SearchResult search(String tags) throws IOException;
+
+  /**
+   * Search for images with the given set of tags.
+   *
+   * @param tags Search query. A space-separated list of tags.
+   * @param pid  Page number. (zero-indexed)
+   * @return A {@link com.cuddlesoft.nori.api.SearchResult} containing a set of Images.
+   * @throws java.io.IOException Network error.
+   */
+  public abstract SearchResult search(String tags, int pid) throws IOException;
+
+  /**
+   * Check if the API server requires or supports optional authentication.
+   * <p/>
+   * This is used in the API server settings activity as follows:
+   * If REQUIRED, the user will need to supply valid credentials.
+   * If OPTIONAL, the credential form is shown, but can be left empty.
+   * If NONE, the credential form will not be shown.
+   *
+   * @return {@link com.cuddlesoft.nori.api.clients.BaseAPIClient.AuthenticationType} value for this API backend.
+   */
+  public abstract AuthenticationType requiresAuthentication();
+
+  /** API authentication types */
+  public enum AuthenticationType {
+    REQUIRED,
+    OPTIONAL,
+    NONE
   }
 
 }
