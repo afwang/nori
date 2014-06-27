f656f64501ecfea14770e3660e455bda863c2179
vomitcuddle <shinku@dollbooru.org>
2014-06-27 06:19:04 +0100

    Refactor BaseAPIClient into interface

---
+++ src/main/java/com/cuddlesoft/nori/api/clients/SearchClient.java
@@ -0,0 +1,56 @@
+/*
+ * This file is part of nori.
+ * Copyright (c) 2014 vomitcuddle <shinku@dollbooru.org>
+ * License: ISC
+ */
+
+package com.cuddlesoft.nori.api.clients;
+
+import com.cuddlesoft.nori.api.SearchResult;
+
+import java.io.IOException;
+
+/**
+ * Interface for a client consuming a Danbooru style API.
+ */
+public interface SearchClient {
+
+  /**
+   * Fetch first page of results containing images with the given set of tags.
+   *
+   * @param tags Search query. A space-separated list of tags.
+   * @return A {@link com.cuddlesoft.nori.api.SearchResult} containing a set of Images.
+   * @throws IOException Network error.
+   */
+  public SearchResult search(String tags) throws IOException;
+
+  /**
+   * Search for images with the given set of tags.
+   *
+   * @param tags Search query. A space-separated list of tags.
+   * @param pid  Page number. (zero-indexed)
+   * @return A {@link com.cuddlesoft.nori.api.SearchResult} containing a set of Images.
+   * @throws java.io.IOException Network error.
+   */
+  public SearchResult search(String tags, int pid) throws IOException;
+
+  /**
+   * Check if the API server requires or supports optional authentication.
+   * <p/>
+   * This is used in the API server settings activity as follows:
+   * If REQUIRED, the user will need to supply valid credentials.
+   * If OPTIONAL, the credential form is shown, but can be left empty.
+   * If NONE, the credential form will not be shown.
+   *
+   * @return {@link com.cuddlesoft.nori.api.clients.SearchClient.AuthenticationType} value for this API backend.
+   */
+  public abstract AuthenticationType requiresAuthentication();
+
+  /** API authentication types */
+  public enum AuthenticationType {
+    REQUIRED,
+    OPTIONAL,
+    NONE
+  }
+
+}
