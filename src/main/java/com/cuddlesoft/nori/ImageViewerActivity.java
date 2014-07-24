57760b3dee29a7b06694fd15d584d42aebb02bfc
vomitcuddle <shinku@dollbooru.org>
2014-07-24 01:54:37 +0100

    Show image metadata in ImageViewerActivity title

--- src/main/java/com/cuddlesoft/nori/ImageViewerActivity.java
+++ src/main/java/com/cuddlesoft/nori/ImageViewerActivity.java
@@ -15,10 +15,18 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 
 import com.cuddlesoft.nori.fragment.ImageFragment;
+import com.cuddlesoft.norilib.Image;
 import com.cuddlesoft.norilib.SearchResult;
+import com.cuddlesoft.norilib.Tag;
 
 /** Activity used to display full-screen images. */
-public class ImageViewerActivity extends ActionBarActivity {
+public class ImageViewerActivity extends ActionBarActivity implements ViewPager.OnPageChangeListener {
+  // TODO: Endless scrolling.
+  // TODO: Pinch to zoom.
+  // TODO: Download image.
+  // TODO: View image on web.
+  // TODO: View image on pixiv.
+  // TODO: Set image as wallpaper.
   /** Identifier used to keep the displayed {@link com.cuddlesoft.norilib.SearchResult} in {@link #onSaveInstanceState(android.os.Bundle)}. */
   private static final String BUNDLE_ID_SEARCH_RESULT = "com.cuddlesoft.nori.SearchResult";
   /** Identifier used to keep the position of the selected {@link com.cuddlesoft.norilib.Image} in {@link #onSaveInstanceState(android.os.Bundle)}. */
@@ -54,7 +62,24 @@ public class ImageViewerActivity extends ActionBarActivity {
     imagePagerAdapter = new ImagePagerAdapter(getSupportFragmentManager());
     viewPager = (ViewPager) findViewById(R.id.image_pager);
     viewPager.setAdapter(imagePagerAdapter);
+    viewPager.setOnPageChangeListener(this);
     viewPager.setCurrentItem(imageIndex);
+  }
+
+  /**
+   * Set the activity title to contain the currently displayed image's metadata.
+   *
+   * @param image Image to get the metadata from.
+   */
+  private void setTitle(Image image) {
+    String title = String.format(getString(R.string.activity_image_viewer_titleFormat),
+        image.id, Tag.stringFromArray(image.tags));
+
+    // Truncate string with ellipsis at the end, if needed.
+    if (title.length() > getResources().getInteger(R.integer.activity_image_viewer_titleMaxLength)) {
+      title = title.substring(0, getResources().getInteger(R.integer.activity_image_viewer_titleMaxLength)) + "…";
+    }
+    setTitle(title);
   }
 
   @Override
@@ -64,6 +89,20 @@ public class ImageViewerActivity extends ActionBarActivity {
     // Keep search result and the index of currently displayed image.
     outState.putParcelable(BUNDLE_ID_SEARCH_RESULT, searchResult);
     outState.putInt(BUNDLE_ID_IMAGE_INDEX, viewPager.getCurrentItem());
+  }
+
+  @Override
+  public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+  }
+
+  @Override
+  public void onPageSelected(int position) {
+    // Set activity title to image metadata.
+    setTitle(searchResult.getImages()[position]);
+  }
+
+  @Override
+  public void onPageScrollStateChanged(int state) {
   }
 
   private class ImagePagerAdapter extends FragmentStatePagerAdapter {
