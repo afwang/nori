90c233639d163caa22ac44bfe130c4aa4b1dc9b5
vomitcuddle <shinku@dollbooru.org>
2014-07-27 22:33:48 +0100

    Define preferences in XML

--- app/src/main/java/com/cuddlesoft/nori/SettingsActivity.java
+++ app/src/main/java/com/cuddlesoft/nori/SettingsActivity.java
@@ -6,8 +6,17 @@
 
 package com.cuddlesoft.nori;
 
+import android.os.Bundle;
 import android.preference.PreferenceActivity;
 
-/** Main settings activity launched from {@link com.cuddlesoft.nori.SearchActivity}. */
+/** Main settings activity managing all the core preferences for the app, launched from {@link com.cuddlesoft.nori.SearchActivity}. */
+@SuppressWarnings("deprecation") // The non-fragment Preferences API is deprecated, but there is no alternative in the support library for API<11 support.
 public class SettingsActivity extends PreferenceActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    addPreferencesFromResource(R.xml.preferences);
+  }
+
 }
