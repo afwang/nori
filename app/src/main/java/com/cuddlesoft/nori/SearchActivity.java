a6b824097820ad28360db6b4c2c699556c4ffb00
vomitcuddle <shinku@dollbooru.org>
2014-07-27 20:05:21 +0100

    Launch SettingsActivity from SearchActivity

--- app/src/main/java/com/cuddlesoft/nori/SearchActivity.java
+++ app/src/main/java/com/cuddlesoft/nori/SearchActivity.java
@@ -173,12 +173,13 @@ public class SearchActivity extends ActionBarActivity implements SearchResultGri
     // Handle action bar item clicks here. The action bar will
     // automatically handle clicks on the Home/Up button, so long
     // as you specify a parent activity in AndroidManifest.xml.
-    /**int id = item.getItemId();
-     if (id == R.id.action_settings) {
-     // TODO: Implement me.
-     return true;
-     }*/
-    return super.onOptionsItemSelected(item);
+    switch (item.getItemId()) {
+      case R.id.action_settings:
+        startActivity(new Intent(SearchActivity.this, SettingsActivity.class));
+        return true;
+      default:
+        return super.onOptionsItemSelected(item);
+    }
   }
 
   @Override
---
+++ app/src/main/java/com/cuddlesoft/nori/SettingsActivity.java
@@ -0,0 +1,13 @@
+/*
+ * This file is part of nori.
+ * Copyright (c) 2014 vomitcuddle <shinku@dollbooru.org>
+ * License: ISC
+ */
+
+package com.cuddlesoft.nori;
+
+import android.preference.PreferenceActivity;
+
+/** Main settings activity launched from {@link com.cuddlesoft.nori.SearchActivity}. */
+public class SettingsActivity extends PreferenceActivity {
+}
