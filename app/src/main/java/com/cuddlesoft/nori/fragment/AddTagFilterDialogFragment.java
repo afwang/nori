58ca8ce2d797e22faf32ce32cf2220c1777dd0e5
vomitcuddle <shinku@dollbooru.org>
2014-09-01 10:49:23 +0100

    Verify tag filters before adding to the database

--- app/src/main/java/com/cuddlesoft/nori/fragment/AddTagFilterDialogFragment.java
+++ app/src/main/java/com/cuddlesoft/nori/fragment/AddTagFilterDialogFragment.java
@@ -21,7 +21,9 @@ import android.widget.EditText;
 import com.cuddlesoft.nori.R;
 
 /** {@link android.support.v4.app.DialogFragment} to let the users add new tags in {@link com.cuddlesoft.nori.TagFilterSettingsActivity}. */
-public class AddTagFilterDialogFragment extends DialogFragment {
+public class AddTagFilterDialogFragment extends DialogFragment implements View.OnClickListener {
+  /** Regular expression pattern used to validate tags before they are added to the database. */
+  private static final String TAG_VALIDATION_PATTERN = "^\\S+$";
   /** Bundle ID used to preserve the entered text and send results to the parent activity. */
   protected static final String BUNDLE_ID_TAG_FILTER = "com.cuddlesoft.nori.TagFilter";
   /** Text box with tag to add to the tag filter list. */
@@ -74,19 +76,10 @@ public class AddTagFilterDialogFragment extends DialogFragment {
     }
 
     // Create the AlertDialog object.
-    return new AlertDialog.Builder(getActivity())
+    final AlertDialog alertDialog = new AlertDialog.Builder(getActivity())
         .setView(view)
-        .setPositiveButton(R.string.action_add, new DialogInterface.OnClickListener() {
-              @Override
-              public void onClick(DialogInterface dialogInterface, int i) {
-                // If entered text isn't empty, send the result to the parent activity.
-                if (listener != null && tagEditText != null && !TextUtils.isEmpty(tagEditText.getText().toString())) {
-                  listener.addTag(tagEditText.getText().toString());
-                  dismiss();
-                }
-              }
-            }
-        ).setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+        .setPositiveButton(R.string.action_add, null)
+        .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
               @Override
               public void onClick(DialogInterface dialogInterface, int i) {
                 // Dismiss dialog.
@@ -94,6 +87,30 @@ public class AddTagFilterDialogFragment extends DialogFragment {
               }
             }
         ).create();
+
+    // onShowListener is used here as a hack to override Android DialogInterface's default onClickInterface
+    // that doesn't provide a way to prevent the dialog from getting dismissed when a button is clicked.
+    alertDialog.setOnShowListener(new DialogInterface.OnShowListener() {
+      @Override
+      public void onShow(DialogInterface dialogInterface) {
+        alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(AddTagFilterDialogFragment.this);
+      }
+    });
+
+    return alertDialog;
+  }
+
+  @Override
+  // Called when the dialog's OK button is clicked.
+  public void onClick(View view) {
+    if (listener != null && tagEditText != null) {
+      // Verify that the entered text isn't empty and doesn't contain illegal whitespace characters.
+      String tag = tagEditText.getText().toString().trim();
+      if (!TextUtils.isEmpty(tag) && tag.matches(TAG_VALIDATION_PATTERN)) {
+        listener.addTag(tagEditText.getText().toString());
+        dismiss();
+      }
+    }
   }
 
   /** Listener interface to be implemented by the Activity or Fragment that receives the tag entered in this Dialog. */
